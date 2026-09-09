37f9aead1e0b6108736bba19bb4680f18c475495
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2026-09-09 13:31:38 +0300

    Prepare Message487 0.0.3 for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "life.andre.message487"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
     }
     signingConfigs {
         if (signingInputs.all { !it.isNullOrBlank() }) {
@@ -49,6 +49,10 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
